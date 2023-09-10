b2f29b5d8296b177b1c0002fd6f8841b4ef93c9d
hanamiJr <hanami_jr@mail.ru>
2023-09-10 18:22:53 +0300

    БД

--- testproject/news/models.py
+++ testproject/news/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+class Articles(models.Model):
+    title = models.CharField('Название', max_length=50, default='Название новости')
+    anons = models.CharField('Анонс', max_length=250)
+    full_text = models.TextField('Текст новости')
+    date = models.DateTimeField('Время новости')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
